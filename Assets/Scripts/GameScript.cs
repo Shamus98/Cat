e1864eee2488b0fc32e040784c7a24f2481d1b10
Shamus98 <G.ida@yandex.ru>
2017-06-28 14:29:20 +0300

    Add files via upload

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameScript : MonoBehaviour
+{
+    public GameObject player;
+    public Text scoreText;
+    public Text gameOverText;
+    public int maxScore;
+    private HeroScript heroScript;
+
+    private void Start()
+    {
+       heroScript = player.GetComponent<HeroScript>();
+    }
+
+    private void Update()
+    {
+        scoreText.text = "Очки: " + heroScript.scores.ToString() + "/" + maxScore.ToString();
+		if (heroScript.scores  >= maxScore && !heroScript.isDead)
+        {
+            gameOverText.text = "Ты победитель сранный нигерррр";
+			StartCoroutine(GoToNextLevel());
+        }
+		if (heroScript.isDead)
+		{
+			gameOverText.text = "Ты сдох, ублюдок";
+			gameOverText.color = new Color (1f, 0.2f, 0);
+			StartCoroutine( Restart());
+		}
+
+    }
+
+    private IEnumerator Restart()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(0);     
+    }
+
+
+	private IEnumerator GoToNextLevel()
+	{
+		yield return new WaitForSeconds(2f);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);     
+	}
+
+
+
+
+
+}
---
+++ Assets/Scripts/MoveOnMouse.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnMouse : MonoBehaviour {
+
+	private Rigidbody2D myBody;
+
+	public float speed;
+
+	void Start () {
+		myBody = GetComponent<Rigidbody2D> ();
+	}
+	
+
+	void Update () {
+		if (!Input.GetMouseButton (0))
+			return;
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector3 destination = Vector3.MoveTowards (transform.position, mousePos, speed);
+		myBody.MovePosition (new Vector2 (destination.x, destination.y));
+	}
+}
